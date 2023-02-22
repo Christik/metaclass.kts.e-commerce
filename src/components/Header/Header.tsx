9a35d45ceb9458a429ff6418cb887dbfbaf56047
Christik <christik.lucid@gmail.com>
2023-02-22 09:02:58 +0300

    feat: реализует шапку сайта

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { ROUTS } from "@config/routs";
+import logo from "@static/img/logo.svg";
+import { NavLink } from "react-router-dom";
+
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <NavLink
+          to={ROUTS.INDEX}
+          className={({ isActive }) =>
+            isActive ? styles.activeLogo : styles.logo
+          }
+        >
+          <img src={logo} width="131" height="42" alt="Lalasia" />
+        </NavLink>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,17 @@
+import Header from "@components/Header";
 import { Outlet } from "react-router-dom";
+
+import styles from "./Layout.module.scss";
 
 const Layout = () => {
   return (
-    <div>
-      <div>Header</div>
-      <Outlet />
-    </div>
+    <>
+      <Header />
+
+      <main className={styles.content}>
+        <Outlet />
+      </main>
+    </>
   );
 };
 

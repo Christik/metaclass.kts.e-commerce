d3ab8f29298e5b5d93ea09408c791fc46623b6d5
Christik <christik.lucid@gmail.com>
2023-02-22 07:51:02 +0300

    feat: реализует компонент Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,41 @@
+import { PropsWithChildren, FC, ButtonHTMLAttributes } from "react";
+
+import Loader, { LoaderSize, LoaderType } from "@components/Loader";
+import classnames from "classnames";
+
+import styles from "./Button.module.scss";
+
+export type ButtonProps = PropsWithChildren<{
+  loading?: boolean;
+}> &
+  ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: FC<ButtonProps> = (props) => {
+  const { className, children, disabled, loading, ...attrs } = props;
+
+  return (
+    <button
+      className={classnames(
+        styles.button,
+        {
+          [styles.disabled]: disabled || loading,
+        },
+        className
+      )}
+      disabled={disabled || loading}
+      {...attrs}
+    >
+      {loading && (
+        <Loader
+          className={styles.loader}
+          size={LoaderSize.s}
+          type={LoaderType.inverted}
+        />
+      )}
+
+      {children}
+    </button>
+  );
+};
+
+export default Button;

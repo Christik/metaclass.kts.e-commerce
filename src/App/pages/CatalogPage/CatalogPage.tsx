8581caf0d69bb6ddccf199a75e1bc99112e30a5c
Christik <christik.lucid@gmail.com>
2023-02-23 20:41:12 +0300

    feat: добавляет заголовок на страницу каталога

--- src/App/pages/CatalogPage/CatalogPage.tsx
+++ src/App/pages/CatalogPage/CatalogPage.tsx
@@ -1,7 +1,23 @@
+import Text from "@components/Text";
+import Title, { TitleSize } from "@components/Title";
+
+import styles from "./CatalogPage.module.scss";
 import Catalog from "./components/Catalog";
 
 const CatalogPage = () => {
-  return <Catalog />;
+  return (
+    <>
+      <header className={styles.header}>
+        <Title size={TitleSize.l}>Products</Title>
+        <Text className={styles.headerText}>
+          We display products based on the latest products we have, if you want
+          to see our old products please enter the name of the item
+        </Text>
+      </header>
+
+      <Catalog />
+    </>
+  );
 };
 
 export default CatalogPage;

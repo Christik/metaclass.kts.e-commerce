53333031610d668cb7a240f48216b268d6d4acd8
Christik <christik.lucid@gmail.com>
2023-02-24 09:42:52 +0300

    feat: стилизует страницу 404

--- src/App/pages/NotFoundPage/NotFoundPage.tsx
+++ src/App/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,17 @@
+import Text from "@components/Text";
+import Title, { TitleSize } from "@components/Title";
+
+import styles from "./NotFoundPage.module.scss";
+
 const NotFoundPage = () => {
-  return <div>404</div>;
+  return (
+    <>
+      <Title className={styles.title} size={TitleSize.l}>
+        Error 404
+      </Title>
+      <Text className={styles.text}>Page not found</Text>
+    </>
+  );
 };
 
 export default NotFoundPage;

e71c2ee2cbd080b500ae9ac9eaa58f296543dffe
Christik <christik.lucid@gmail.com>
2023-03-04 17:46:06 +0300

    feat: реализует компонент MultiDropdown

---
+++ src/components/MultiDropdown/MultiDropdown.tsx
@@ -0,0 +1,97 @@
+import { FC, useEffect, useId, useState } from "react";
+
+import classnames from "classnames";
+
+import styles from "./MultiDropdown.module.scss";
+
+export type Option = {
+  key: string;
+  value: string;
+};
+
+export type MultiDropdownProps = {
+  options: Option[];
+  value: Option[];
+  onChange: (value: Option[]) => void;
+  disabled?: boolean;
+  pluralizeOptions: (value: Option[]) => string;
+};
+
+const MultiDropdown: FC<MultiDropdownProps> = (props) => {
+  const { options, value, disabled, pluralizeOptions, onChange } = props;
+  const id = useId();
+  const [isOpened, setIsOpened] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState(value);
+
+  const handleClick = () => {
+    setIsOpened((prevState) => !prevState);
+  };
+
+  const handleCheckboxChange = (option: Option) => {
+    setSelectedOptions((prevSelectedOptions) => {
+      if (prevSelectedOptions.some(({ key }) => key === option.key)) {
+        const updatedOptions = prevSelectedOptions.filter(
+          ({ key }) => key !== option.key
+        );
+
+        onChange(updatedOptions);
+        return updatedOptions;
+      }
+
+      onChange([option]);
+      return [...prevSelectedOptions, option];
+    });
+  };
+
+  useEffect(() => setSelectedOptions(value), [value]);
+
+  useEffect(() => setIsOpened(false), [disabled]);
+
+  return (
+    <div className={styles["multi-dropdown"]}>
+      <button
+        type="button"
+        className={classnames(styles.header, {
+          [styles["header_disabled"]]: disabled,
+        })}
+        disabled={disabled}
+        onClick={handleClick}
+      >
+        {pluralizeOptions(selectedOptions)}
+      </button>
+
+      {isOpened && (
+        <ul className={styles.list}>
+          {options.map((option, index) => {
+            const isChecked = selectedOptions.some(
+              ({ key }) => key === option.key
+            );
+
+            return (
+              <li key={option.key} className={styles.option}>
+                <input
+                  type="checkbox"
+                  className={styles.checkbox}
+                  id={`${id}-${index}`}
+                  checked={isChecked}
+                  onChange={() => handleCheckboxChange(option)}
+                />
+
+                <label
+                  className={classnames(styles.label, {
+                    [styles["label_selected"]]: isChecked,
+                  })}
+                  htmlFor={`${id}-${index}`}
+                >
+                  {option.value}
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default MultiDropdown;

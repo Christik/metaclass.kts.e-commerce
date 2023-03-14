e07d7ec1a2a5fffe1b91bfab14729934ab1b6228
Christik <christik.lucid@gmail.com>
2023-03-14 13:05:14 +0300

    fix: обновляет ссылку в логотипе

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { FC, memo } from "react";
 
 import { ROUTS } from "@config/routs";
 import logo from "@static/img/logo.svg";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import styles from "./Header.module.scss";
 
@@ -10,14 +10,9 @@ const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
-        <NavLink
-          to={ROUTS.INDEX}
-          className={({ isActive }) =>
-            isActive ? styles["active-logo"] : styles.logo
-          }
-        >
+        <Link to={ROUTS.INDEX} className={styles.logo}>
           <img src={logo} width="131" height="42" alt="Lalasia" />
-        </NavLink>
+        </Link>
       </div>
     </header>
   );
